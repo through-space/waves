d2d54d54af1fb3f728866d23dab24f22503a9898
lvil <mysecretemail@gmail.com>
2025-01-13 20:07:29 +0200

    Added path aliasing. Added SimpleComponent

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { SimpleComponent } from "@components/atoms/SimpleComponent/SimpleComponent";
 
 function App() {
 	const [count, setCount] = useState<number>(0);
@@ -41,6 +42,7 @@ function App() {
 			<p className="read-the-docs">
 				Click on the Vite and React logos to learn more
 			</p>
+			<SimpleComponent>I am a child of SimpleComponent</SimpleComponent>
 		</>
 	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@components": path.resolve(__dirname, "./src/components"),
+			"@assets": path.resolve(__dirname, "./src/assets"),
+			"@hooks": path.resolve(__dirname, "./src/hooks"),
+			"@config": path.resolve(__dirname, "./src/config"),
+			"@services": path.resolve(__dirname, "./src/services"),
+		},
+	},
+	plugins: [react({})],
+});
