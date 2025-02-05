5eacb2b3317657ca3d7c1e99c104f1392362306d
lvil <mysecretemail@gmail.com>
2025-02-05 18:02:06 +0200

    Added Tailwind. Added Wavechart component

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-			"@app": path.resolve(__dirname, "./src/app"),
-			"@assets": path.resolve(__dirname, "./src/assets"),
-			"@components": path.resolve(__dirname, "./src/components"),
-			"@features": path.resolve(__dirname, "./src/features"),
-			"@hooks": path.resolve(__dirname, "./src/hooks"),
-			"@config": path.resolve(__dirname, "./src/config"),
-			"@services": path.resolve(__dirname, "./src/services"),
-		},
-	},
-	plugins: [react({})],
+	plugins: [react({}), tailwindcss(), tsconfigPaths()],
 });
