46686e829d2e8274af6914140ad05ae12b271788
lvil <mysecretemail@gmail.com>
2025-02-10 22:32:13 +0200

    added

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { MainLayout } from "@components/layouts/MainLayout/MainLayout";
-import { WaveList } from "@features/waves/components/WaveList";
+import { WaveList } from "@features/waves/components/organisms/WaveList/WaveList";
 
 export const HomePage: FC = () => {
 	return (
